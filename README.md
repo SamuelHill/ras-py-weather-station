30557bdadad0cda7caf300d7ea0f01000b819dae
SAMHILLPC\Sam Hill <whatinthesamhill@protonmail.com>
2022-08-26 00:05:34 -0500

    Better documentation
    readme and docstrings plus and image of the deployed station

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# ras-py-weather-station
- Indoor weather station with LCD output and logging to InfluxDB for visualization in Graphana
+# Raspberry Pi Weather Station
+
+Indoor weather station built for continuous operation on a Raspberry Pi 4 (4GB). Uses 3 Sensors;
+
+- BMP-085 for temperature and pressure (both measured and adjusted for the know sea level at the location the station is deployed)
+- DHT-22 for temperature and humidty
+- Light dependent resistor for light level (charge_time_limit=0.1 and multiplied by 100 : semi-normalized to the room lighting)
+
+This data is printed to an LCD screen using a few custom characters for better stylization; to make the display both dense and informative. There is also a custom message function that can be used to add the occasional "I love you" to the LCD for a significant other or the like.
+
+As well as being printed out, the data is logged into an InfluxDB instance running locally on the pi. This allows for some fun visualization in Graphana!
+
+![The breadboard and cobbler station](station-breadboard.jpg)
+
+## Installs
+
+### Needed for InfluxDB;
+
+    pip install influxdb-client
+
+Also needs an Influx instance running locally so see setting up [InfluxDB on a Pi](https://pimylifeup.com/raspberry-pi-influxdb/) for more.
+
+### 'Needed' for the various connections from the Raspberry Pi to the station;
+
+    pip install Adafruit-BMP
+    pip install adafruit-circuitpython-dht
+    pip install adafruit-python-shell
+    pip install gpiozero
+    pip install RPi.GPIO
+    pip install RPLCD
+
+Check [this link on circuitpython](https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi) for more about the setup needed for this library.
--- weather-station.py
+++ weather-station.py
@@ -1,23 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Filename:    weather-station.py
+# @Author:      Samuel Hill
+# @Email:       whatinthesamhill@protonmail.com
+
+"""Indoor weather station built for continuous operation on a Raspberry Pi 4.
+    Uses a DHT-22, BMP-085, and light dependent resistor (LDR) to collect
+    temperature, humidity, pressure, and light level data. Prints data to an
+    LCD screen (with some custom characters for better stylization/display) and
+    logs the data to an InfluxDB instance running locally on the pi."""
+
 from time import sleep
 from datetime import datetime, timedelta
 from statistics import mean
+from influxdb_client import InfluxDBClient, Point
+from Adafruit_BMP import BMP085  # DEPRECATED, still works for now...
+from adafruit_dht import DHT22
 from board import MISO as DHT_PIN, D6 as LDR_PIN, D22 as NPN_PIN, \
                   D25 as PIN_RS, D24 as PIN_E, D26 as PIN_D4, \
                   D13 as PIN_D5, D12 as PIN_D6, D16 as PIN_D7
-from RPi.GPIO import BCM, cleanup, setup, OUT, HIGH, output
 from gpiozero import LightSensor
-from adafruit_dht import DHT22
-from Adafruit_BMP import BMP085
+from RPi.GPIO import BCM, cleanup, setup, OUT, HIGH, output
 from RPLCD import CharLCD
-from influxdb_client import InfluxDBClient, Point
 
-# Timings:
 REFRESH = 2.0
 MSG_DURATION = 10.0
 MSG_INTERVAL = timedelta(minutes=20)
 LAST_MSG_TIME = datetime.now()
 
-# Sensors:
 ldrSensor = LightSensor(LDR_PIN.id, charge_time_limit=0.1)
 setup(NPN_PIN.id, OUT, initial=HIGH)  # DHT sensor is finicky, transistor fix
 sleep(1.0)  # just giving it time to setup
@@ -30,7 +40,6 @@ data = {'dht_temp': 0, 'humidity': 0, 'bmp_temp': 0,
 LATEST_PRESSURE = []
 LATEST_SAMPLES = 5400  # 3h = 180m = 10800s / REFRESH = 5400
 
-# Out:
 lcd = CharLCD(cols=20, rows=4, pin_rs=PIN_RS.id, pin_e=PIN_E.id,
               pins_data=[PIN_D4.id, PIN_D5.id, PIN_D6.id, PIN_D7.id],
               numbering_mode=BCM)
@@ -46,27 +55,44 @@ lcd.create_char(7, (0b00000, 0b01010, 0b10101, 0b10001,  # heart
                     0b01010, 0b00100, 0b00000, 0b00000))
 lcd.clear()
 
-URL = 'http://localhost:8086'  # influx db
+URL = 'http://localhost:8086'
 USERNAME, PASSWORD = 'admin', 'admin'
 TOKEN = f'{USERNAME}:{PASSWORD}'
 DATABASE, RETENTION_POLICY = 'homebridgeWeather', 'autogen'
 BUCKET = f'{DATABASE}/{RETENTION_POLICY}'
 
 
+def _write_new_line(row: int, to_write: str):
+    lcd.cursor_pos = (row, 0)
+    lcd.write_string(to_write)
+
+
 def custom_message():
-    global LAST_MSG_TIME
+    """Checks if a MSG_INTERVAL duration of time has passed since LAST_MSG_TIME
+        then posts a custom message for MSG_DURATION amount of time."""
+    # need the globals to modify the properly scoped variables
+    global LAST_MSG_TIME  # pylint: disable=global-statement
     if datetime.now() - LAST_MSG_TIME > MSG_INTERVAL:
         lcd.clear()
-        lcd.cursor_pos = (1, 0)
-        lcd.write_string('     I love you')
-        lcd.cursor_pos = (2, 0)
-        lcd.write_string('      Monica \x07')
+        _write_new_line(1, '     I love you')
+        _write_new_line(2, '      Monica \x07')
         LAST_MSG_TIME = datetime.now()
         sleep(MSG_DURATION)
 
 
+# pylint: disable=inconsistent-return-statements
 def update_data():
-    global DHT_SUCCESS, LATEST_PRESSURE
+    """Attempts to collect data from the DHT-22 sensor (toggling power if the
+        read fails). Also collects data from both the BMP-085 sensor and the
+        light dependent resistor (LDR). All of the collected data is stored in
+        the data dictionary defined in this script (overwritten like a global).
+        The pressure data is as added to a list of length LATEST_SAMPLES for
+        later calculation of the pressure change.
+
+    Returns:
+        datetime: The datetime.now() call from the beginning of the update"""
+    # need the globals to modify the properly scoped variables
+    global DHT_SUCCESS, LATEST_PRESSURE  # pylint: disable=global-statement
     beginning_of_update = datetime.now()
     try:
         data['dht_temp'] = dhtSensor.temperature
@@ -91,6 +117,13 @@ def update_data():
 
 
 def store_data(database, time):
+    """Takes the recently updated data dictionary from update_data and logs
+    everything into the local InfluxDB instance.
+
+    Args:
+        database (InfluxDBClient): the client you are using to write to the db
+        time (datetime): the time the new data was collected
+    """
     def send_to_influx(ptype, tag, field, pdata):
         point = Point(ptype).tag('source', tag).field(field, pdata).time(time)
         database.write(bucket=BUCKET, record=point)
@@ -105,31 +138,27 @@ def store_data(database, time):
 
 
 def print_data_to_screen():
+    """Prints the collected data out to the LCD screen"""
     def percent(value):
         return f'{value:.1f}%' if value < 100.0 else '100%'
 
     def temps(celsius):
         return f'{celsius:.1f}/{(celsius * (9 / 5) + 32):.0f}\x00C/F'
 
-    def direction():
-        if len(LATEST_PRESSURE) > 60:
-            p_diff = mean(LATEST_PRESSURE[-30:]) - mean(LATEST_PRESSURE[:30])
-            return '^^' if p_diff >= 500 else '^' if p_diff >= 250 else '/' \
-                if p_diff >= 25 else '-' if p_diff >= -25 else '\x03' \
-                if p_diff >= -250 else 'v' if p_diff > -500 else 'vv'
-        return '-'
+    direction = '-'
+    if len(LATEST_PRESSURE) > 60:
+        p_diff = mean(LATEST_PRESSURE[-30:]) - mean(LATEST_PRESSURE[:30])
+        direction = '^^' if p_diff >= 500 else '^' if p_diff >= 250 else '/' \
+            if p_diff >= 25 else '-' if p_diff >= -25 else '\x03' \
+            if p_diff >= -250 else 'v' if p_diff > -500 else 'vv'
 
     lcd.clear()
-    lcd.cursor_pos = (0, 0)
-    lcd.write_string(f'{percent(data["humidity"])} \x01, '
-                     f'{temps(data["dht_temp"])}')
-    lcd.cursor_pos = (1, 0)
-    lcd.write_string(f'{percent(data["light_level"])} \x02, '
-                     f'{temps(data["bmp_temp"])}')
-    lcd.cursor_pos = (2, 0)
-    lcd.write_string(f'Actual: {data["pressure"] / 1000:.3f} kPa')
-    lcd.cursor_pos = (3, 0)
-    lcd.write_string(f'Sea: {data["sea_level"] / 1000:.3f} kPa {direction()}')
+    _write_new_line(0, f'{percent(data["humidity"])} \x01, '
+                       f'{temps(data["dht_temp"])}')
+    _write_new_line(1, f'{percent(data["light_level"])} \x02, '
+                       f'{temps(data["bmp_temp"])}')
+    _write_new_line(2, f'Actual: {data["pressure"] / 1000:.3f} kPa')
+    _write_new_line(3, f'Sea: {data["sea_level"] / 1000:.3f} kPa {direction}')
 
 
 if __name__ == '__main__':
